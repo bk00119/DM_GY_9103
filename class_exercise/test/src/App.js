19fbf32ad30beb6dbeae0c9962645f1a0fbad8dc
Brian Kim <39502789+bk00119@users.noreply.github.com>
2023-09-14 15:31:00 -0400

    week2 class exercise done

--- class_exercise/test/src/App.js
+++ class_exercise/test/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
+import Test from '@components/Test'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Test />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
