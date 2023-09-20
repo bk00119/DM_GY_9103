968983051a8d850ed73a7b53d11573a48779c03e
Brian Kim <39502789+bk00119@users.noreply.github.com>
2023-09-20 13:20:00 -0400

    completed class exercise sep 20

---
+++ class_exercise/week_3/src/RecipeCard/RecipeImg.js
@@ -0,0 +1,7 @@
+import React from 'react'
+// import './styles.css'
+import styles from "./RecipeCard.module.css"
+
+export default function RecipeImg(props) {
+  return <img src={props.imgSrc} alt="buttermilk pancakes" className={styles.recipe_img} />
+}
